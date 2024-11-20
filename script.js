d2b700c1cf55d9ff75fbdd51dbca829bba3ffd4e
HemanthChirikonda <67725821+HemanthChirikonda@users.noreply.github.com>
2024-11-20 15:34:39 -0500

    codes added

---
+++ script.js
@@ -0,0 +1,68 @@
+// variables
+// define the variables using certain keywords : var 
+// var variableName = variableValue;
+
+
+
+ var number =190;
+
+// print statement in javascript is => console.log(output);
+console.log(number); // 190
+
+
+
+
+number =100;
+console.log(number);  // 100
+
+
+// create a two variables and store the sum of two variables in another variable and print it.
+
+// how to create a variable ? 
+// keyword   variableName     = variableValue;
+   var       number2          = 24;
+   var       number3          = 8;
+   var       result1          = number2 + number3;
+
+   console.log(result1);
+
+   var   result2 = number2 - number3;
+
+   console.log(result2);
+
+
+   var result3 = number2 * number3;
+
+   console.log(result3);
+
+   // keyword const 
+
+
+   const value1 = 200;
+
+   console.log(value1);
+
+
+   // Data Types 
+
+// string => ""
+// number => its simple numbers  => 1, 2, 3, -7,-10
+// NaN  => NOT a Number
+// Null => empty
+// undefined => its not existed
+// boolean => true or false.
+
+console.log("a"/10);
+
+result2 = null;
+
+console.log(result2);
+
+var firstName = "Adams";
+var lastName  = "Morgan";
+
+var fullName  = firstName + " "+ lastName;
+// print the full name
+console.log(fullName);
+
+
